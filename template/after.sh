197b87504da8fbbb0c2fbeaf3264bf9431b4033d
Torfinn Nome <torfinn.nome@usit.uio.no>
2026-03-24 12:54:40 +0100

    Add support for CryoSPARC v5

--- template/after.sh
+++ template/after.sh
@@ -1,7 +1,8 @@
 ## Wait for the CryoSPARC server to start
 echo "Waiting for CryoSPARC server to open port ${port}..."
 echo "TIMING - Starting wait at: $(date)"
-if wait_until_port_used "${host}:${port}" 600; then
+# Increased timeout to 3600s for CryoSPARC v5 database upgrade which can take up to 1h
+if wait_until_port_used "${host}:${port}" 3600; then
   echo "Discovered CryoSPARC server listening on port ${port}!"
   echo "TIMING - Wait ended at: $(date)"
 else
